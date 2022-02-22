96612fa14723178d388d56d3070404837f0f721c
Niki Wix Skaarup <NikiSkaarup@users.noreply.github.com>
2022-02-22 21:18:50 +0100

    write-output to write-host

--- init.ps1
+++ init.ps1
@@ -1,44 +1,44 @@
 $repoPath = Split-Path $MyInvocation.MyCommand.Path -Parent
-$binPath = Join-Path repoPath 'bin'
+$binPath = Join-Path $repoPath 'bin'
 
-Write-Output -f Green "Running init"
+Write-Host -f Green "Running init"
 $setupStart = Invoke-Expression "$binPath\get-date-formatted.ps1"
 
-Write-Output -f Cyan "Set execution policy"
-Write-Output -f Cyan "Completed installing Scoop"
+Write-Host -f Cyan "Set execution policy"
+Write-Host -f Cyan "Completed installing Scoop"
 
-Write-Output -f Cyan "Installing Scoop"
+Write-Host -f Cyan "Installing Scoop"
 Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
-Write-Output -f Cyan "Completed installing Scoop"
+Write-Host -f Cyan "Completed installing Scoop"
 
-Write-Output -f Cyan "Adding buckets to scoop"
+Write-Host -f Cyan "Adding buckets to scoop"
 scoop bucket add extras
 scoop bucket add nerd-fonts
 scoop bucket add versions
-Write-Output -f Cyan "Completed adding buckets to scoop"
+Write-Host -f Cyan "Completed adding buckets to scoop"
 
-Write-Output -f Cyan "Installing Scoop apps"
+Write-Host -f Cyan "Installing Scoop apps"
 scoop install 7zip git
 scoop install aria2 scoop-search
 scoop install sudo pshazz rainbow figlet which pwsh
 scoop install posh-git starship
 scoop install vscode sublime-text sublime-merge
 scoop install qbittorrent madvr mpc-hc-fork mpv-git youtube-dl ffmpeg
-Write-Output -f Cyan "Completed installing Scoop apps"
+Write-Host -f Cyan "Completed installing Scoop apps"
 
-Write-Output -f Cyan "Adding repo persistenly to path"
+Write-Host -f Cyan "Adding repo persistenly to path"
 Invoke-Expression "$repoPath\add-bin-persistenly-to-path.ps1"
-Write-Output -f Cyan "Added repo persistenly to path"
+Write-Host -f Cyan "Added repo persistenly to path"
 
 Write-Host -f Red "Entering administrative powershell"
 sudo pwsh -File "$repopath\sudo-init.ps1"
 Write-Host -f Red "Left administrative powershell"
 
-Write-Output -f Cyan "Restoring persisted configurations"
+Write-Host -f Cyan "Restoring persisted configurations"
 Invoke-Expression "$binPath\restore-config.ps1"
-Write-Output -f Cyan "Restored persisted configurations"
+Write-Host -f Cyan "Restored persisted configurations"
 
 $setupEnd = Invoke-Expression "$binPath\get-date-formatted.ps1"
-Write-Output -f Cyan "Started on: $setupStart" 
-Write-Output -f Cyan "Ended on: $setupEnd" 
-Write-Output -f Green "Completed init"
+Write-Host -f Cyan "Started on: $setupStart" 
+Write-Host -f Cyan "Ended on: $setupEnd" 
+Write-Host -f Green "Completed init"
--- sudo-init.ps1
+++ sudo-init.ps1
@@ -1,13 +1,13 @@
-Write-Output -f Cyan "Entered administrative powershell"
+Write-Host -f Cyan "Entered administrative powershell"
 
-Write-Output -f Cyan "Installing Scoop fonts"
+Write-Host -f Cyan "Installing Scoop fonts"
 scoop install JetBrains-Mono JetBrainsMono-NF-Mono JetBrainsMono-NF
 scoop install Cascadia-Code CascadiaCode-NF CascadiaCode-NF-Mono
-Write-Output -f Cyan "Completed installing Scoop fonts"
+Write-Host -f Cyan "Completed installing Scoop fonts"
 
-Write-Output -f Cyan "Setup profile.ps1 symlink"
+Write-Host -f Cyan "Setup profile.ps1 symlink"
 Invoke-Expression "$repoPath\symlink-profile.ps1"
-Write-Output -f Cyan "Completed setup profile.ps1 symlink"
+Write-Host -f Cyan "Completed setup profile.ps1 symlink"
 
 Write-Host -f Cyan "Enabling SSH-Agent"
 Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
@@ -21,4 +21,4 @@ Write-Host -f Cyan "Disable bell sound"
 Set-Service beep -StartupType disabled
 Write-Host -f Cyan "Disabled bell sound"
 
-Write-Output -f Red "Leaving administrative powershell"
+Write-Host -f Red "Leaving administrative powershell"
