06fa7b172bbc98964fdfd456edcfa3eca0cc63cb
Niki Wix Skaarup <NikiSkaarup@users.noreply.github.com>
2022-02-22 21:51:16 +0100

    Corrected which Environment path is changed

--- add-bin-persistenly-to-path.ps1
+++ add-bin-persistenly-to-path.ps1
@@ -1,8 +1,8 @@
 $binPath = Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) 'bin'
 
-$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name Path).path
 $newpath = $oldpath + ";" + $binPath
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name Path -Value $newPath
 
 if (!($env:Path -split ";" -contains $binPath)) {
     # Add the bin dir to the path for current session
